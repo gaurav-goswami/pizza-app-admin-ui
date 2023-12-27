ce565c83dc4f60497ccf22e45923b45baf3d0729
Gaurav Goswami <gauravgoswami.aug@gmail.com>
2023-12-27 19:28:04 +0530

    update login form ui

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -1,15 +1,57 @@
+import { Button, Card, Checkbox, Form, Input, Layout, Space } from "antd";
+import { LockFilled, UserOutlined, LockOutlined  } from "@ant-design/icons";
+
 const Login = () => {
   return (
     <>
-        <h1>Sign In</h1>
+      {/* <h1>Sign In</h1>
         <input type="text" placeholder="Username" />
         <input type="password" placeholder="Password" />
         <button>Log In</button>
         <label htmlFor="remember-me">Remember Me</label>
         <input type="checkbox" id="remember-me" />
-        <a href="#">Forgot Password</a>
-    </>
-  )
-}
+        <a href="#">Forgot Password</a> */}
 
-export default Login
+      <Layout
+        style={{ height: "100vh", display: "grid", placeItems: "center" }}
+      >
+        <Card
+          bordered={false}
+          title={
+            <Space
+              style={{ width: "100%", fontSize: 16, justifyContent: "center" }}
+            >
+              <LockFilled />
+              Sign In
+            </Space>
+          }
+          style={{ width: 300 }}
+        >
+          <Form>
+            <Form.Item name="username">
+              <Input prefix={<UserOutlined />} placeholder="Username" />
+            </Form.Item>
+            <Form.Item name="password">
+              <Input.Password prefix={<LockOutlined />} placeholder="Password" />
+            </Form.Item>
+            <Form.Item name="remember">
+              <Checkbox>Remember me</Checkbox>
+              <a href="">Forgot password</a>
+            </Form.Item>
+            <Form.Item>
+              <Button
+                type="primary"
+                htmlType="submit"
+                style={{ width: "100%" }}
+              >
+                Log in
+              </Button>
+            </Form.Item>
+          </Form>
+        </Card>
+      </Layout>
+    </>
+  );
+};
+
+export default Login;
--- src/router.tsx
+++ src/router.tsx
@@ -1,9 +1,14 @@
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "./pages/HomePage";
+import Login from "./pages/login/Login";
 
 export const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <HomePage />,
   },
+  {
+    path: '/login',
+    element: <Login />
+  }
 ]);
