8608401d5ab11cbda5454454cb33e4504f222271
Gaurav Goswami <gauravgoswami.aug@gmail.com>
2024-01-01 19:30:38 +0530

    route protection

--- src/router.tsx
+++ src/router.tsx
@@ -3,26 +3,33 @@ import HomePage from "./pages/HomePage";
 import Login from "./pages/login/Login";
 import Dashboard from "./Layouts/Dashboard";
 import NonAuth from "./Layouts/NonAuth";
+import Root from "./Layouts/Root";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <Dashboard />,
+    element: <Root />,
     children: [
       {
-        path: "/",
-        element: <HomePage />,
+        path: "",
+        element: <Dashboard />,
+        children: [
+          {
+            path: "",
+            element: <HomePage />,
+          },
+        ],
+      },
+      {
+        path: "/auth",
+        element: <NonAuth />,
+        children: [
+          {
+            path: "login",
+            element: <Login />,
+          },
+        ],
       },
     ],
-  },
-  {
-    path: '/auth',
-    element: <NonAuth />,
-    children: [
-      {
-        path: "login",
-        element: <Login />,
-      },
-    ]
   },
 ]);
