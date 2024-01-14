e126dcf91b1c80cc2edf0d42d53bc71689486947
Gaurav Goswami <gauravgoswami.aug@gmail.com>
2024-01-14 15:38:12 +0530

    add tenants table

--- src/pages/ProtectedRoutes/Restaurants/Restaurants.tsx
+++ src/pages/ProtectedRoutes/Restaurants/Restaurants.tsx
@@ -1,6 +1,9 @@
 import { useQuery } from "@tanstack/react-query";
 import { getTenants } from "../../../http/apis/api";
 import { throwErrorMessage } from "../../../utils/methods";
+import { Table, Typography } from "antd";
+import { COLORS } from "../../../styles/theme";
+import dayjs from "dayjs";
 
 const getAllTenants = async () => {
   try {
@@ -12,6 +15,52 @@ const getAllTenants = async () => {
   }
 };
 
+const { Text } = Typography;
+const tenantColumns = [
+  {
+    title: "id",
+    dataIndex: "id",
+    key: "id",
+    fixed: "left",
+    width: 150,
+  },
+  {
+    title: "Tenant name",
+    dataIndex: "name",
+    key: "name",
+    render: (text: string) => {
+      return <Text style={{ color: COLORS.COLOR_PRIMARY }}>{text}</Text>;
+    },
+  },
+  {
+    title: "Tenant address",
+    dataIndex: "address",
+    key: "address",
+  },
+  {
+    title: "Created At",
+    dataIndex: "createdAt",
+    key: "createdAt",
+    render: (text: string) => {
+      return (
+        <Text style={{ color: COLORS.COLOR_PRIMARY }}>
+          {dayjs(text).format("DD MMM YYYY")}
+        </Text>
+      );
+    },
+  },
+  {
+    title: "Updated At",
+    dataIndex: "updatedAt",
+    key: "updatedAt",
+    render: (text: string) => {
+      return (
+        <Text>{dayjs(text).format('DD MMM YYYY')}</Text>
+      )
+    }
+  },
+];
+
 const Restaurants = () => {
   const { data: tenantsList, isLoading: tenantsLoading } = useQuery({
     queryKey: ["tenants"],
@@ -22,7 +71,13 @@ const Restaurants = () => {
 
   return (
     <>
-
+      {tenantsLoading && <div>Loading...</div>}
+      <Table
+        columns={tenantColumns}
+        dataSource={tenantsList}
+        scroll={{ x: 1300 }}
+        rowKey={"id"}
+      />
     </>
   );
 };
---
+++ src/pages/ProtectedRoutes/Restaurants/types.ts
@@ -0,0 +1,7 @@
+export interface ITenantData {
+  id: number;
+  name: string;
+  address: string;
+  createdAt: string;
+  updatedAt: string;
+}
